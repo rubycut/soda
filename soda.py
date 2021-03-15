5d0f04fd03944d2f95c60f7cd9afec874f711b4f
Hrvoje Marjanovic <hrvoje.marjanovic@gmail.com>
2021-03-15 19:42:24 +0100

    Error out if url is not valid.

--- soda.py
+++ soda.py
@@ -19,15 +19,17 @@ if (type(url)() is None):
    sys.exit(
        "Gitlab url not found in config.ini, see config-sample.ini for more info.")
 
-print("Token is: ", token)
-
 gitlab_url = f'{url}/api/v4/issues?state=opened&private_token={token}'
 
-with urllib.request.urlopen(gitlab_url) as response:
-   raw_json = response.read()
-   json = json.loads(raw_json)
-   print("Number of Gitlab open issues: ", len(json))
-   
+try:
+   with urllib.request.urlopen(gitlab_url) as response:
+      raw_json = response.read()
+      json = json.loads(raw_json)
+      print("Number of Gitlab open issues: ", len(json))
+except:
+   sys.exit(
+       f'Can\'t connect to: {gitlab_url}')
+
 # fetch number of errors on loki
 
 # fetch number of unassigned email on front
